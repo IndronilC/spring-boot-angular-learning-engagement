1501a92ba8c544f6779467e43816e5445a863e95
IndronilC <chakraborty.indranil@gmail.com>
2026-03-27 13:58:28 +0530

    feat: Committing the Code which carries augmentation of the activate-account page so that user post activation can go to login

--- customer-portal/src/app/auth/components/activate/activate.ts
+++ customer-portal/src/app/auth/components/activate/activate.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth';
@@ -8,7 +8,7 @@ import { AuthService } from '../../services/auth';
 @Component({
   selector: 'app-activate',
   standalone: true,
-   imports: [FormsModule, CommonModule],
+   imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './activate.html',
   styleUrl: './activate.scss'
 })
@@ -19,19 +19,23 @@ export class ActivateComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     console.log("🔥 ActivateComponent loaded");
     const token = this.route.snapshot.queryParamMap.get('token');
     console.log("🔍 Activation token:", token);
+   
     if (token) {
       this.auth.activate(token).subscribe({
        next: () => {
         console.log("✅ API success");
         this.message = 'Account activated successfully!';
         this.isError = false;
+        console.log("isError", this.isError);
+        console.log("message", this.message);
       },
       error: () => {
         console.log("❌ API error");
@@ -41,4 +45,7 @@ export class ActivateComponent implements OnInit {
       });
     }
   }
+  goToLogin() {
+  this.router.navigate(['/login']);
+}
 }
